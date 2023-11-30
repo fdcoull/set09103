f084655f162deadc39830116ed081597678b11bf
F D Coull <58175445+fdcoull@users.noreply.github.com>
2023-11-30 18:00:00 +0000

    Add chapter 8

---
+++ chapter-8/8-1-config.py
@@ -0,0 +1,40 @@
+import configparser
+
+from flask import Flask
+
+app = Flask(__name__)
+
+def init(app):
+    config = configparser.ConfigParser()
+    try:
+        print("INIT FUNCTION")
+        config_location = "etc/defaults.cfg"
+        config.read(config_location)
+
+        app.config['DEBUG'] = config.get("config", "debug")
+        app.config['ip_address'] = config.get("config", "ip_address")
+        app.config['port'] = config.get("config", "port")
+        app.config['url'] = config.get("config", "url")
+    except:
+        print("Could not read configs from: ", config_location)
+
+init(app)
+
+@app.route('/')
+def root():
+    return "Hello Napier from the configuration testing app"
+
+@app.route('/config/')
+def config():
+    s = []
+    s.append('debug:'+str(app.config['DEBUG']))
+    s.append('port:'+app.config['port'])
+    s.append('url:'+app.config['url'])
+    s.append('ip_address:'+app.config['ip_address'])
+    return ', '.join(s)
+
+if __name__ == '__main__':
+    init(app)
+    app.run(
+            host=app.config['ip_address'],
+            port=int(app.config['port']))
